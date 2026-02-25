7476f1464928c60b53ef266dd7465ab125cd9e70
dornakv <vojtechdornak@gmail.com>
2026-02-25 16:27:36 +0100

    v1.0.2

--- Formula/openvpn-interactive-darwin.rb
+++ Formula/openvpn-interactive-darwin.rb
@@ -1,8 +1,8 @@
 class OpenvpnInteractiveDarwin < Formula
   desc "Interactive OpenVPN management script for macOS with keychain integration"
   homepage "https://github.com/dornakv/openvpn-interactive-darwin"
-  url "https://github.com/dornakv/openvpn-interactive-darwin/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "a24a65d8e49b1a92453a20ad1a29e63417cbb0eafb36ad486f9195c469bc88c2"
+  url "https://github.com/dornakv/openvpn-interactive-darwin/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "315bc5d515de8032a6cb0ad3f0f4b4ae4450b4635bdb7de2fdad2470577c47d2"
   license :unlicense
 
   depends_on "openvpn"
